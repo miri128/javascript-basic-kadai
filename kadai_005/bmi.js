ab6a705a7095a574034af00045226c1f62136d64
qstoki <seto_sgm@outlook.com>
2025-02-26 16:40:33 +0900

    Add files via upload

---
+++ kadai_005/bmi.js
@@ -0,0 +1,10 @@
+let height;
+let bodyWeight;
+let bodyMassIndex;
+
+height = 1.7;
+bodyWeight = 68;
+bodyMassIndex = 23.529411764705884;
+
+console.log(bodyMassIndex);
+console.log(bodyWeight / (height * height));
